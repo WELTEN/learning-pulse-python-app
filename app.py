6dc3e637e221361c5e7a49247cf951c7b6c886ea
dimstudio <daniele@dimstudio.org>
2015-11-06 14:09:44 +0100

    Further changes

--- app.py
+++ app.py
@@ -40,7 +40,11 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 DEFAULT_DASHBOARD_NAME = 'default_dashboard'
 
-working_time = [8,9,10,11,12,13,14,15,16,17,18]
+timeframes = [7,8,9,10,11,12,13,14,15,16,17,18]  
+todayNice = datetime.utcnow().strftime('%A %d, %b %Y') #e.g. Tuesday 03, Nov 2015]
+to = 1 # Time offset UTC+1 
+today = datetime.now() # dateobject 
+
 
 # We set a parent key on the 'Ratings' to ensure that they are all
 # in the same entity dashboard. Queries across the single entity dashboard
@@ -76,12 +80,11 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         dashboard_name = self.request.get('user', DEFAULT_DASHBOARD_NAME)
         
-        ratings_query = Rating.query(ancestor=dashboard_key(dashboard_name)).order(-Rating.date)
+        ratings_query = Rating.query().order(-Rating.date)
         ratings = ratings_query.fetch(10)
-        timeframes = [7,8,9,10,11,12,13,14,15,16,17,18]  
-        to = 1 #offset UTC+1 s      
+
         currentHour = int(datetime.utcnow().strftime('%H')) #e.g. 9 
-        todayNice = datetime.utcnow().strftime('%A %d, %b %Y') #e.g. Tuesday 03, Nov 2015
+        
         user = users.get_current_user() 
         listTimeframes = ""
         if user:
@@ -90,23 +93,24 @@ class MainPage(webapp2.RequestHandler):
             
             for i in timeframes:
                 if i < currentHour:
-                    """
-                    timeframe = datetime.utcnow().strftime('%Y-%m-%d '+ str(i) +':00:00')
-                    date_object = datetime.strptime(timeframe, '%Y-%m-%d %H:%M:%S')
+                    today = datetime.utcnow().strftime('%Y-%m-%d')
+                    date_object = datetime.strptime(today, '%Y-%m-%d')
                     query = Rating.query(
-                        ndb.AND(Rating.date >= date_object),
-                                Rating.date < date_object + timedelta(hours=1))
-                    """
-                    query = Rating.query(Rating.timeframe == str(i+to))
+                        ancestor=dashboard_key(dashboard_name),
+                        filters=ndb.AND(Rating.date >= date_object,
+                                        Rating.date < date_object + timedelta(days=1),
+                                        Rating.timeframe == str(i+to))
+                    )
+                    
                     if not query.get():
-                        listTimeframes += "<li class='list-group-item'><span class='badge'>"+str(i+to)+ \
-                        " - "+str(i+to+1)+" </span> <a href='#"+str(i+to)+"' class='timeframe-rating'>Rate this timeframe</a></li>"
+                        listTimeframes += "<a href='#"+str(i+to)+"' class='list-group-item timeframe-rating'><span class='badge'>"+str(i+to)+ \
+                        " - "+str(i+to+1)+" </span> Rate this timeframe</a>"
                     else:
-                        listTimeframes += "<li class='list-group-item list-group-item-success'>"+"<span class='badge'>"+ \
-                        str(i+to)+" - "+str(i+to+1)+" </span> You rated this timeframe already</li>"
+                        listTimeframes += "<a href='#"+str(i+to)+"' class='list-group-item list-group-item-success'>"+"<span class='badge'>"+ \
+                        str(i+to)+" - "+str(i+to+1)+"</span> You rated this timeframe already</>"
                 elif i==currentHour:
-                    listTimeframes += "<li class='list-group-item list-group-item-warning'>"+"<span class='badge'>"+ \
-                        str(i+to)+" - "+str(i+to+1)+" </span> Ongoing timeframe</li>"
+                    listTimeframes += "<a href='#"+str(i+to)+"' class='list-group-item list-group-item-warning disabled'>"+"<span class='badge'>"+ \
+                        str(i+to)+" - "+str(i+to+1)+"</span> Ongoing timeframe</a>"
                         
         else:
             url = users.create_login_url(self.request.uri)
@@ -150,15 +154,44 @@ class MainPage(webapp2.RequestHandler):
 class Visualisation(webapp2.RequestHandler):
     def get(self):
         dashboard_name = self.request.get('user', DEFAULT_DASHBOARD_NAME)
-        ratings_query = Rating.query(ancestor=dashboard_key(dashboard_name)).order(-Rating.date)
-        ratings = 
-        for r in ratings_query.fetch():
-        #    
-        dailychart = "['data1', 30, 200, 100, 400, 150, 250],"
-        template_values = {
-            'user': user,
-            'dailychart': dailychart,
-        }
+        user = users.get_current_user() 
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout' 
+            today = datetime.utcnow().strftime('%Y-%m-%d')
+            date_object = datetime.strptime(today, '%Y-%m-%d')
+            query = Rating.query(
+                ancestor=dashboard_key(dashboard_name),
+                filters=ndb.AND(Rating.date >= date_object,
+                                Rating.date < date_object + timedelta(days=1))
+            )
+            chart_x_label = "['x',"
+            chart_productivity = "['Productivity',"
+            chart_stress = "['Stress',"
+            chart_challenge = "['Challenge',"
+            chart_abilities = "['Abilities',"
+            for r in query.fetch():
+                chart_x_label += r.timeframe + ","
+                chart_productivity += r.productivity + ","
+                chart_stress += r.stress + ","
+                chart_challenge += r.challenge + "," 
+                chart_abilities += r.abilities + ","
+            chart_x_label += "]"
+            chart_productivity += "]"
+            chart_stress += "]"
+            chart_challenge += "]"
+            chart_abilities += "]"
+
+            chart_data = chart_x_label + "," + chart_productivity + "," + chart_stress + "," + chart_challenge + "," + chart_abilities
+            template_values = {
+                'user': user,
+                'chart_data': chart_data,
+                'url_linktext': url_linktext,
+                'today': todayNice,
+            }
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
 
         template = JINJA_ENVIRONMENT.get_template('vis.html')
         self.response.write(template.render(template_values))
@@ -174,4 +207,5 @@ class Login(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/', Login),
     ('/rate', MainPage),
+    ('/vis', Visualisation),
 ], debug=True)
